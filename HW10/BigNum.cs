0b241a93fd08b481d9c4d37799de9e1c8725bfab
elliotclawrence <elliotclawrence@gmail.com>
2016-11-03 21:25:43 -0700

    Fixed some more stuff, added some more stuff.

--- HW10/BigNum.cs
+++ HW10/BigNum.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
+using System.Collections;
 
 namespace CS422
 {
@@ -11,11 +12,105 @@ namespace CS422
     {
         
         private BigInteger baseInt;
-        private BigInteger exponentInt;
-        
+        private int exponent;
+        private bool isNegative;
+        private bool isUndefined;
 
         public BigNum(string number)
         {
+            initializeWithString(number);
+        }
+
+        public BigNum(double value, bool useDoubleToString) //
+        {
+            if (useDoubleToString)
+            {
+                string number = value.ToString();
+                
+            } //double.tostring() end
+
+            else //Cannot use double.tostring()
+            {
+                if (Double.IsNaN(value))
+                {
+                    baseInt = new BigInteger();
+                    exponent = 0;
+
+                    isUndefined = true;
+
+                    return;
+                }
+
+               
+                byte[] byteArray = BitConverter.GetBytes(value);
+                var bits = new BitArray(byteArray);
+                
+                
+                for (int x = 0; x < 32; x ++) //bit array needs to be reversed on windows.
+                {
+                    bool curBit = bits[x];
+                    bits[x] = bits[bits.Length - x - 1];
+                    bits[bits.Length - x - 1] = curBit;
+                }
+
+
+                bool isPositive = true;
+                int localexponent = 0;
+                
+
+                if (bits[0] == true)
+                    isPositive = false;
+
+                string exponentStr = "";
+                for (int x = 1; x < 12; x++)//populate bits into a string
+                {
+                    if (bits[x] == true)
+                        exponentStr += "1";
+                    else
+                        exponentStr += "0";
+                }
+
+                localexponent = Convert.ToInt32(exponentStr, 2) - 1023; //exponent (math from wikipedia)
+
+                string fractionStr = "";
+
+                for (int x = 12; x < 64; x++) //populate bits into a string
+                {
+                    if (bits[x] == true)
+                        fractionStr += "1";
+                    else
+                        fractionStr += "0";
+                }
+
+                decimal dec = 1;
+                for (int x = 1; x <= 52; x++)
+                {
+                    if (fractionStr[x-1] == '1') // if bit is true
+                    { 
+                        dec += (decimal)Math.Pow(2, -x);
+                    }
+                }
+
+                string actualNum = "";
+
+                if (isPositive == false)
+                {
+                    actualNum += "-";
+                }
+
+                 actualNum += (dec * (decimal)Math.Pow(2, localexponent)).ToString();
+
+
+                initializeWithString(actualNum);
+            }
+        }
+
+        private void initializeWithString(string number)
+        {
+            isUndefined = false; //if we're here, you can assume the BigNum is defined.
+
+
+            int x = 0;
             char[] validStart = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' };
             if (number == null || number == "")
                 throw new Exception();
@@ -27,15 +122,14 @@ namespace CS422
             bool encounteredDecimal = false;
             int decimalLocation = -1;
 
-            for (int x = 0; x < number.Length; x++)
+            for (x = 0; x < number.Length; x++)
             {
                 if (number[x] == '-')
                 {
                     if (x > 0) //if x is greater than 0, no '-' allowed.
                         throw new Exception();
-
                 }
-                    
+
                 else if (number[x] == '.')
                 {
                     if (encounteredDecimal == false) //first decimal is ok.
@@ -43,11 +137,10 @@ namespace CS422
                         encounteredDecimal = true;
                         decimalLocation = x;
                     }
-                        
+
                     else                             //second decimal is not ok.
                         throw new Exception();
                 }
-                 
 
                 else if (number[x] < '0' || number[x] > '9') //if it's not a '-' or '.' it must be 0-9 , if not, throw exception.
                     throw new Exception();
@@ -55,9 +148,10 @@ namespace CS422
             }
             //valid string.
 
+            x = 0;
 
             string numberNoDecimal = "";
-            int x = 0;
+
             while (x < number.Length)
             {
                 if (number[x] != '.')
@@ -65,89 +159,27 @@ namespace CS422
                 x++;
             }
 
+            baseInt = new BigInteger(); //get a new integer
+            baseInt = 0;
+
+            BigInteger multiplyCoEfficient = new BigInteger();
+            multiplyCoEfficient = 1; 
+
+            for (x = numberNoDecimal.Length -1; x >= 0; x--)
+            {
+                multiplyCoEfficient += (numberNoDecimal[x] - '0') * multiplyCoEfficient;
+                multiplyCoEfficient *= 10;
+            }
+
+
             if (!encounteredDecimal) //if no decimal
             {
-                baseInt = new BigInteger(long.Parse(numberNoDecimal));
-                exponentInt = new BigInteger(0);
+                exponent = 0;
             }
 
             else //there was a decimal
             {
-                baseInt = new BigInteger(long.Parse(numberNoDecimal));
-                exponentInt = new BigInteger((number.Length - decimalLocation) * -1);
-            }
-        }
-
-        public BigNum(double value, bool useDoubleToString) //
-        {
-            if (useDoubleToString)
-            {
-                string number = value.ToString();
-                char[] validStart = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' };
-                if (number == null || number == "")
-                    throw new Exception();
-                if (!validStart.Contains(number[0])) //check to make sure the first char is a valid starting character
-                    throw new Exception();
-                if (number.Contains(" ")) //check for white space
-                    throw new Exception();
-
-                bool encounteredDecimal = false;
-                int decimalLocation = -1;
-
-                for (int x = 0; x < number.Length; x++)
-                {
-                    if (number[x] == '-')
-                    {
-                        if (x > 0) //if x is greater than 0, no '-' allowed.
-                            throw new Exception();
-
-                    }
-
-                    else if (number[x] == '.')
-                    {
-                        if (encounteredDecimal == false) //first decimal is ok.
-                        {
-                            encounteredDecimal = true;
-                            decimalLocation = x;
-                        }
-
-                        else                             //second decimal is not ok.
-                            throw new Exception();
-                    }
-
-
-                    else if (number[x] < '0' || number[x] > '9') //if it's not a '-' or '.' it must be 0-9 , if not, throw exception.
-                        throw new Exception();
-
-                }
-                //valid string.
-
-
-                string numberNoDecimal = "";
-                int x = 0;
-                while (x < number.Length)
-                {
-                    if (number[x] != '.')
-                        numberNoDecimal += number[x];
-                    x++;
-                }
-
-                if (!encounteredDecimal) //if no decimal
-                {
-                    baseInt = new BigInteger(long.Parse(numberNoDecimal));
-                    exponentInt = new BigInteger(0);
-                }
-
-                else //there was a decimal
-                {
-                    baseInt = new BigInteger(long.Parse(numberNoDecimal));
-                    exponentInt = new BigInteger((number.Length - decimalLocation) * -1);
-                }
-            } //double.tostring() end
-
-            else
-            {
-
+                exponent = ((numberNoDecimal.Length - decimalLocation) * -1);
             }
         }
 
@@ -155,9 +187,9 @@ namespace CS422
         {
             string number = baseInt.ToString();
 
-            if (exponentInt != 0)
+            if (exponent != 0)
             {
-                number.Insert((number.Length - 1) - (int) exponentInt, ",");
+                number.Insert((number.Length - 1) -  exponent, ",");
             }
 
             return number;
@@ -167,7 +199,7 @@ namespace CS422
         {
             get
             {
-                return true; //change this
+                return isUndefined; //change this
             }
         }
 
--- HW10/Program.cs
+++ HW10/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW10
+namespace CS422
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BigNum teddy = new BigNum(4.999, false);
         }
     }
 }
